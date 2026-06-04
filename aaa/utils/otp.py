22adf2ff96ca6335bd655b7bf70bf359637ed426
mejomba <moojaminzadeh@gmail.com>
2026-06-04 21:35:52 +0330

    update

--- aaa/utils/otp.py
+++ aaa/utils/otp.py
@@ -23,6 +23,7 @@ class OTPAction:
 
     @staticmethod
     def _send_sms(message: str, phones: List):
+        message = f'جهت تست و فاقد اعتبار، کد تایید شماره همراه: {message}'
         for phone in phones:
             import json
             json_data = json.dumps({
